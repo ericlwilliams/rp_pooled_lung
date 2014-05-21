0227aeaccb68777f7bfa5f85b7c6c5b03cda737a
Eric Williams <ericlwilliams@gmail.com>
2014-05-21 14:07:44 -0400

    finished pooled results printing script

--- RP_Pooled_Results.m
+++ RP_Pooled_Results.m
@@ -1,17 +1,18 @@
-function [ output_args ] = RP_Pooled_Results()
+function  RP_Pooled_Results()
 %RP_Pooled_Results
 %   Prints LKB results of pooled analysis
 %   Used for table 1 in paper
 
 % Need all data
 fn='Z:/elw/MATLAB/meta_analy/meta_data/MSK_NKI_UMich_RTOG_fine_EUD_fine_meta.mat';
+pld_data = {'CGnki','CGmsk','CGrtog','CGum','CGcomb'};
 
 if isunix %on mac
     fn = strrep(fn,'Z:/elw/','/Users/elw/Documents/');
 end
 
-pld_data = {'CGnki','CGmsk','CGrtog','CGum','CGcomb'};
-protocols = {'NKI','MSK','RTOG','UM','Comb'};
+
+protocols = {'NKI','MSK','RTOG','UMich','Comb'};
 
 for i=1:length(pld_data)
     CGdata=load(fn,pld_data{i});
@@ -25,36 +26,57 @@ for i=1:length(pld_data)
     % find current best parameters
     llhd_mx = max(CGdata.mLymanGrid.loglikelihood(:));
     
+    
+    llhd_n = max(max(CGdata.mLymanGrid.loglikelihood,[],2),[],1);
+    llhd_a = flipdim(squeeze(llhd_n),1);
     llhd_td50 = max(max(CGdata.mLymanGrid.loglikelihood,[],3),[],2);
     llhd_m = max(max(CGdata.mLymanGrid.loglikelihood,[],3),[],1);
-    llhd_n = max(max(CGdata.mLymanGrid.loglikelihood,[],2),[],1);
     
-    low68 = llhd_mx -0.5*(chi2inv(0.68,3));
-    low95 = llhd_mx -0.5*(chi2inv(0.95,3));
-%   low99 = mx_llhd -0.5*(chi2inv(0.99,3));
+    
+    % Fan's method (assumes df=1), underestimate confidence intervals     
+     low68 = llhd_mx-0.5* (1);
+     low95 = llhd_mx-0.5* (1.96^2);
+
+%      low68 = llhd_mx -0.5*(chi2inv(0.68,3));
+%      low95 = llhd_mx -0.5*(chi2inv(0.95,3));
 
     % 68% point of td50
     [td50, td50_68ci] = ConfidenceInterval(CGdata.mLymanGrid.TD50,llhd_td50, low68);
     [~, td50_95ci] = ConfidenceInterval(CGdata.mLymanGrid.TD50,llhd_td50, low95);
+    
+    [m, m_68ci] = ConfidenceInterval(CGdata.mLymanGrid.m,llhd_m, low68);
+    [~, m_95ci] = ConfidenceInterval(CGdata.mLymanGrid.m,llhd_m, low95);
+    
+     [n, n_68ci] = ConfidenceInterval(CGdata.mLymanN,llhd_n, low68);
+     [~, n_95ci] = ConfidenceInterval(CGdata.mLymanN,llhd_n, low95);
+
+     [a, a_68ci] = ConfidenceInterval(CGdata.mLymanN,llhd_a, low68);    
+     [~, a_95ci] = ConfidenceInterval(CGdata.mLymanN,llhd_a, low95);
+        
+    
+    
     disp(['== ',cur_data,' ==']);
+    disp(['n (68%) [95%]']);
+    disp([num2str(n),' (',num2str(n_68ci(1),4),' - ',num2str(n_68ci(2),4),')',...
+                        ' [',num2str(n_95ci(1),4),' - ',num2str(n_95ci(2),4),']']);
+    disp('-');
+    disp(['a (68%) [95%]']);
+    disp([num2str(a),' (',num2str(a_68ci(1),4),' - ',num2str(a_68ci(2),4),')',...
+                        ' [',num2str(a_95ci(1),4),' - ',num2str(a_95ci(2),4),']']);
+    disp('-');
+    
     disp(['TD50 (68%) [95%]']);
     disp([num2str(td50),' (',num2str(td50_68ci(1),4),' - ',num2str(td50_68ci(2),4),')',...
                         ' [',num2str(td50_95ci(1),4),' - ',num2str(td50_95ci(2),4),']']);
-    
-    
-%     disp([num2str(td50),' - ',num2str(td50_ci)]);
+    disp('-');
+    disp(['m (68%) [95%]']);
+    disp([num2str(m),' (',num2str(m_68ci(1),4),' - ',num2str(m_68ci(2),4),')',...
+                        ' [',num2str(m_95ci(1),4),' - ',num2str(m_95ci(2),4),']']);
+    disp('-');
 
+            
 
     
-    
-%     b0sd(k) = min(CI(2)-b0(k), b0(k)-CI(1));
-%     % 68% point of b1
-%     [b1(k), CI] = ConfidenceInterval(OCobjs(k).mLymanGrid.m,likely2, CI68);
-%     b1sd(k) = min(CI(2)-b1(k), b1(k)-CI(1));
-%     % 68% point of a
-%     [a(k), CI] = ConfidenceInterval(OCobjs(k).mLymanN,likely3, CI68);
-%     asd(k) = min(CI(2)-a(k), a(k)-CI(1));
-
 end
 
 end
